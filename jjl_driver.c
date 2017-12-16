00429fc99f435daee4328d9a6dbb98835e8211b8
James Lawson <jameslawson@users.noreply.github.com>
2017-12-16 02:56:35 +0000

    Add character device file

--- jjl_driver.c
+++ jjl_driver.c
@@ -8,36 +8,41 @@ MODULE_AUTHOR("James Lawson");
 
 // [1]: kernel-space data about the state of the device
 // [2]: use file_operations struct to bind device driver functions to the device file,
-//      so that io system calls the device file like read() and write() will call *our* functions
+//      so that io system calls on the device file like read() and write()
+//      will call through to *our* functions
 dev_t dev_nums;
-/* struct jjl_device { */
-/*     struct cdev cdev;      // stores kernel character device info */
-/* }; */
-/* struct jjl_device jjl_dev; */
-/* struct file_operations jjl_fops = { .owner = THIS_MODULE, .open  = jjl_open, .read  = jjl_read, .release = jjl_release, }; */
-/* int jjl_open(struct inode *inode, struct file *filp) { return 0; } */
-/* int jjl_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) { return 0; } */
-/* int jjl_release(struct inode *inode, struct file *filp) { return 0; } */
+struct jjl_device {
+    struct cdev cdev;      // stores kernel character device info
+};
+struct jjl_device jjl_dev;
+
+int jjl_open(struct inode *inode, struct file *filp) { return 0; }
+int jjl_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) { return 0; }
+int jjl_release(struct inode *inode, struct file *filp) { return 0; }
+struct file_operations jjl_fops = {
+    .owner = THIS_MODULE,
+    .open  = jjl_open,
+    .read  = jjl_read,
+    .release = jjl_release
+};
 
 
 static int __init jjl_driver_register(void) {
     // request new (major, minor) device numbers
+    // request kernel create a new character device kernel data structure
     printk(KERN_INFO "[jjl] Loading Module 'jjl_driver'...");
     if (alloc_chrdev_region(&dev_nums, 0, 1, "jjl")) {
         printk(KERN_NOTICE "could not allocate device numbers\n");
         return -1;
     }
 
-    /* if (cdev_init(jjl_dev.cdev, &jjl_fops)) { */
-    /*     printk(KERN_WARNING "could not allocate new character device"); */
-    /*     return -1; */
-    /* } */
-    /* jjl_dev.ops = &jjl_fops; */
-    /* jjl_dev.owner = THIS_MODULE; */
-    /* if (cdev_add(&dev->cdev, devno, 1)) { */
-    /*     printk(KERN_NOTICE "could not add newly allocated character device"); */
-    /*     return -1; */
-    /* } */
+    cdev_init(&(jjl_dev.cdev), &jjl_fops);
+    jjl_dev.cdev.ops = &jjl_fops;
+    jjl_dev.cdev.owner = THIS_MODULE;
+    if (cdev_add(&(jjl_dev.cdev), dev_nums, 1)) {
+        printk(KERN_NOTICE "could not add newly allocated character device");
+        return -1;
+    }
 
     printk(KERN_INFO "[jjl] Allocated device numbers (major, minor) = (%d,%d)\n", MAJOR(dev_nums), MINOR(dev_nums));
     printk(KERN_INFO "[jjl] Inserted Module 'jjl_driver'");
@@ -45,9 +50,10 @@ static int __init jjl_driver_register(void) {
 }
 
 static void __exit jjl_driver_unregister(void) {
-    // free device numbers that alloc_chrdev_region originally allocated for us
-    /* cdev_del(&jjl_dev->cdev); */
-    unregister_chrdev_region(MAJOR(dev_nums), 1);
+    // free up the kernel data for our chracter device
+    // free up the device numbers that alloc_chrdev_region originally allocated for us
+    cdev_del(&(jjl_dev.cdev));
+    unregister_chrdev_region(dev_nums, 1);
     printk(KERN_INFO "Removed Module 'jjl_driver'\n");
 }
 
--- load_driver.sh
+++ load_driver.sh
@@ -17,8 +17,8 @@ major=$(awk "\$2==\"$device\" {print \$1}" /proc/devices)
 # remove any stale files in case unload_driver.sh was not correctly called
 # then create a fresh file
 dev_file="/dev/${device}"
-rm -f dev_file
-mknod dev_file c $major 0
+rm -f $dev_file
+mknod $dev_file c $major 0
 
 # give appropriate permissions/ownership to the newly created device file
 # everyone needs read access. for all linux special files, the owner
@@ -28,5 +28,5 @@ mknod dev_file c $major 0
 # if its not there assume we're using wheel instead
 group="staff"
 grep -q '^staff:' /etc/group || group="wheel"
-chgrp $group dev_file
-chmod 664 dev_file
+chgrp $group $dev_file
+chmod 664 $dev_file
